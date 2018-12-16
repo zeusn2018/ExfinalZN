faea7bfcee2e799eb271693aca2ae1fa0ea167da
zeusn2018 <nunja.zeus@gmail.com>
2018-12-16 14:33:21 -0500

    strategy

---
+++ ExamenFinal/Pregunta3/IStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenFinal.Pregunta3
+{
+    interface IStrategy
+    {
+        bool HasDuplicateValues(int[] numbers);
+    }
+}
